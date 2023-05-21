1a43ddd3a4e79226381f23564a5ec6afef307ab4
farjanaIslam09 <farjana.jatri@gmail.com>
2023-05-21 13:12:48 +0600

    added year progrss box

--- src/components/ProgressSlider.vue
+++ src/components/ProgressSlider.vue
@@ -47,7 +47,8 @@ const readMoreSlider = ref([
 
 const firstElement = ref(false);
 const lastElement = ref(false);
-const swiperIndex = ref(0)
+const swiperIndex = ref(0);
+const progressYear = ref(2019)
 
 const swiperCarousel = ref(true)
 
@@ -76,36 +77,38 @@ const slidePrev = () => {
 
 onMounted(() => {
   swiperCarousel.value.on('slideNextTransitionEnd',()=>{
-    let progressBarFill = document.querySelector('.color-bar');
+    let progressBarFill = document.querySelector('.fill-color-bar');
+    let yearProgressWrap = document.querySelector('.year-progress-wrap');
+    let yearProgress = document.querySelector('.year-progress');
+ 
     let progressBarWidth = swiperIndex.value += 1
     if(progressBarWidth <= 4){
       progressBarFill.style.width = `${(25 * progressBarWidth)}%`
-      
+      yearProgress.style.width = `${((25 * progressBarWidth) + 3.5)}%`
+      yearProgressWrap.style.margin = '0 24px'
+      progressYear.value = progressYear.value += 1
     }
     let nodeElement = document.getElementsByClassName('circle-wrap')[0]
     let circle = nodeElement.children[progressBarWidth - 1]
     circle.classList.add('change-color')
 
-    let yearIndicatorBox = document.querySelector('.year-indicator-box');
-    if(swiperIndex.value === 1){
-      yearIndicatorBox.style.left = '21.9%'
-    }
-    if(swiperIndex.value === 2){
-      yearIndicatorBox.style.left = '46.9%'
-    }
-    if(swiperIndex.value === 3){
-      yearIndicatorBox.style.left = '71.9%'
-    }
-    if(swiperIndex.value === 4){
-      yearIndicatorBox.style.left = '96.9%'
-    }
   });
 
   swiperCarousel.value.on('slidePrevTransitionEnd',()=>{
-    let progressBarFill = document.querySelector('.color-bar');
+    let progressBarFill = document.querySelector('.fill-color-bar');
+    let yearProgressWrap = document.querySelector('.year-progress-wrap');
+    let yearProgress = document.querySelector('.year-progress');
+
     let progressBarWidth = swiperIndex.value -= 1
+    progressYear.value = progressYear.value -= 1
     if(progressBarWidth >= 0 && progressBarWidth <= 4){
       progressBarFill.style.width = `${(25 * progressBarWidth)}%`
+      if(progressBarWidth === 0){
+        yearProgress.style.width = '90px'
+        yearProgressWrap.style.margin = '0'
+      }else{
+        yearProgress.style.width = `${((25 * progressBarWidth) + 3.5)}%`
+      }
     }
     let nodeElement = document.getElementsByClassName('circle-wrap')[0]
     let circle = nodeElement.children[progressBarWidth]
@@ -157,15 +160,27 @@ onMounted(() => {
             </div>
           </div>
 
-      <div class="mt-20 mx-6">
-        <div class="progress-bar relative h-4 w-full bg-[#FDE5E2] rounded-[15px]">
-          <div class="circle-wrap flex h-4 ">
-            <div class="w-1/4"></div>
-            <div class="w-1/4"></div>
-            <div class="w-1/4"></div>
-            <div class="w-1/4"></div>
+      <div class="mt-20 relative">
+        <div class="year-progress-wrap">
+          <div class="year-progress flex justify-end">
+            <div>
+              <div class="bg-[#F04935] text-white rounded-full font-semibold w-[90px] h-[44px] flex justify-center items-center">{{ progressYear }}</div>
+              <div class="flex justify-center"><img src="../assets/images/svg/fill-arrow-down.svg" alt=""></div>
+            </div>
           </div>
-          <div class="color-bar"></div>
+        </div>
+      </div>
+      <div class="mt-2 relative">
+        <div class="mx-6">
+          <div class="progress-bar relative h-4 w-full bg-[#FDE5E2] rounded-[15px]">
+            <div class="circle-wrap flex h-4 ">
+              <div class="w-1/4"></div>
+              <div class="w-1/4"></div>
+              <div class="w-1/4"></div>
+              <div class="w-1/4"></div>
+            </div>
+            <div class="fill-color-bar"></div>
+          </div>
         </div>
       </div>
       </div>
@@ -177,6 +192,61 @@ onMounted(() => {
 :root {
   --progressbar-width: 0%;
 }
+/* .year-progress-wrap div {
+  position: relative;
+  z-index: 2;
+  transition: color .3s ease-in-out;
+}
+
+.year-progress-wrap::before{
+  content: "2019";
+  padding: 8px 16px;
+  color: white;
+  position: absolute;
+  z-index: -1;
+  background: #F04935;
+  transition: .3s ease-in-out;
+  border-radius: 2rem;
+  right: -25px;
+  top: -45px;
+
+} */
+/* .year-progress-wrap div:first-child::after{
+  content: "2019";
+  color: white;
+  padding: 8px 16px;
+  color: white;
+  position: absolute;
+  z-index: -1;
+  background: #F04935;
+  transition: .3s ease-in-out;
+  border-radius: 2rem;
+  left: -20px;
+  top: -45px;
+} */
+.year-progress{
+width: 90px;
+transition: all 0.2s ease;
+margin-left: -15px;
+}
+
+/* .year-progress-bar {
+  transition: all 0.2s ease;
+}
+.year-progress-bar::before{
+  content: "";
+    width: 0;
+    height: 0;
+    border-left: 8px solid transparent;
+    border-right: 8px solid transparent;
+    border-top: 8px solid #F04935;
+    left: 50px;
+    position: absolute;
+    left: 24px;
+    top: 40px;
+    transition: all 0.3s ease;
+} */
+
 .circle-wrap div {
   position: relative;
   z-index: 2;
@@ -236,7 +306,7 @@ onMounted(() => {
   object-fit: cover;
 }
 
-.color-bar{
+.fill-color-bar{
     position: absolute;
     background-color: rgba(240, 73, 53, 0.7);
     width: 16px;
@@ -246,20 +316,5 @@ onMounted(() => {
     top: 0;
     /* animation: progres 4s linear;     */
 }
-.year-indicator-box{
-  transition: all 0.3s ease;
-}
-.year-indicator-box::before{
-  content: "";
-    width: 0;
-    height: 0;
-    border-left: 8px solid transparent;
-    border-right: 8px solid transparent;
-    border-top: 8px solid #F04935;
-    left: 50px;
-    position: absolute;
-    left: 29px;
-    top: 44px;
-    transition: all 0.3s ease;
-}
+
 </style>
