34f0292f99f3fe3a9c8b751f46c4bba0fd3e09b3
farjanaIslam09 <farjana.jatri@gmail.com>
2023-06-14 12:40:46 +0600

    going back to 8bd2937

---
+++ src/components/OldSlider.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { FreeMode} from 'swiper';
+import { computed, onMounted, ref } from 'vue';
+const modules = [FreeMode];
+</script>
+
+<style scoped>
+
+</style>
