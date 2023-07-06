16e3737c490bf2354f5705f391b3ac47b340942d
unknown <farjana09>
2023-07-06 16:48:55 +0600

    current opened role section done

--- src/components/CareerHeader.vue
+++ src/components/CareerHeader.vue
@@ -1,18 +1,45 @@
 <template>
-
+    <div class=" py-10 md:py-14 xl:py-20 mt-10">
+        <div class="custom-container">
+            <h2 class="text-[28px] md:text-4xl font-medium">Current open roles</h2>
+            <div class="mt-8 md:mt-10 grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
+                <div v-for="role in openedRoles" :key="role.id" class="p-6 rounded-2xl border border-1-[#DBDBDB]">
+                    <div class="lg:flex items-center justify-between">
+                        <div>
+                            <p class="text-xl font-semibold">{{ role.title }}</p>
+                            <div class="my-2 flex gap-2 items-center">
+                                <p class="text-base font-medium text-[#4D4D4F]">Salary: <span>{{ role.salary }}</span></p>
+                                <div class="h-[6px] w-[6px] bg-[#DBDBDB] rounded-full"></div>
+                                <p class="text-base font-medium text-[#4D4D4F]">{{ role.employment_type }}</p>
+                            </div>
+                            <p class="text-base font-medium text-[#4D4D4F]">Application deadline: <span v-html="role.deadline"></span></p>
+                        </div>
+                        <div class="lg:pt-0 pt-4">
+                            <router-link to="" class="flex justify-center items-center gap-2 h-10 w-[130px] rounded-full border border-[#DBDBDB] bg-[#DBDBDB] lg:bg-white">
+                                <span class="inline-block text-sm leading-5 font-medium">View details</span>
+                                <div><img src="/images/arrow-right.svg" alt=""></div>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import careerData from '../data/career.js';
+console.log(careerData, 'value');
+const openedRoles = ref([])
+
+// onMounted(() => {
+//     openedRoles.value = careerData
+// })
+openedRoles.value = careerData
 
 </script>
 
 <style scoped>
-.benefit-header::before{
-    content: url('/images/perks.svg');
-    position: absolute;
-    height: 50px;
-    width: 51px;
-    top: -20px;
-    right: -46px;
-}
+
 </style>
--- src/data/career.js
+++ src/data/career.js
@@ -1,11 +1,246 @@
-export const careerData = [
+const careerList = [
     {
-        id: 1,
-        role: '',
-        salary: '',
-        jobNature: '',
-        deadline: '',
-        location: '',
-        vacancy: 2
+        "title": "Software Engineer (Vue.Js)",
+        "slug": "software-engineer-vuejs",
+        "iconPath": "job_circular/team_lead.jpg",
+        "deadline": "25<sup>th</sup> June, 2023",
+        "description":
+            "<p class=\"font-thin body_text mt-4\"><u>Job Responsibilities:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Clear concept of Vue lifecycle & proven working experience with APIs</li>\n" +
+            "<li>Following the best practices and standards for development</li>\n" +
+            "<li>Debugging applications, monitoring, and fixing errors</li>\n" +
+            "<li>Creating reusable code and infrastructure for future use</li>\n" +
+            "<li>Collaborating with the other developers and the testing team, mentoring and training developers where appropriate</li>\n" +
+            "<li>Staying informed about new technologies</li>\n" +
+            "<li>Must have expertise in Bootstrap, Tailwind & responsive design.</li>\n" +
+            "<li>Experience in Version Control (GIT) is a must</li>\n" +
+            "<li>Strong problem-solving skills</li>\n" +
+            "<li>Knowledge of RESTful API integration and effective</li>\n" +
+            "<li>Please do not apply without having at least one years of professional experience in VueJS / ReactJS / AngularJS</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Education & Experience:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>B.Sc. in Computer Science & Engineering / Software Engineering or equivalent degree</li>\n" +
+            "<li>Educational background is flexible for a highly skilled person</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Benefits:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Lunch</li>\n" +
+            "<li>Festival bonus</li>\n" +
+            "<li>Yearly salary review</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Number of vacancy</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>01</li>\n" +
+            "</ul>",
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Clear concept of Vue lifecycle & proven working experience with APIs</li>\n" +
+            "<li>Following the best practices and standards for development</li>\n" +
+            "<li>Debugging applications, monitoring, and fixing errors</li>\n" +
+            "<li>Creating reusable code and infrastructure for future use</li>\n" +
+            "<li>Collaborating with the other developers and the testing team, mentoring and training developers where appropriate</li>\n" +
+            "<li>Staying informed about new technologies</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "30,000 - 60,000",
+        "location": "Gulshan-2, Dhaka",
+        "mail_to": "career@jatri.co",
     },
+    {
+        "title": "Software Engineer (Node.Js)",
+        "slug": "software-engineer-nodejs",
+        "iconPath": "job_circular/team_lead.jpg",
+        "deadline": "25<sup>th</sup> June, 2023",
+        "description":
+            "<p class=\"font-thin body_text mt-4\"><u>Job Responsibilities:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Expert knowledge of NodeJS - ExpressJS & NestJS</li>\n" +
+            "<li>Highly skilled in NoSQL databases especially in MongoDB</li>\n" +
+            "<li>Use data modeling skills to enhance DB and Cloud Storage layer</li>\n" +
+            "<li>Develop design and testing documentation</li>\n" +
+            "<li>Expert understanding of GIT and Github</li>\n" +
+            "<li>Ability to work autonomously and as part of a team</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Education & Experience:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>B.Sc. in Computer Science & Engineering / Software Engineering or equivalent degree</li>\n" +
+            "<li>Educational background is flexible for a highly skilled person</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Additional Requirements:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Experience on AWS is a plus</li>\n" +
+            "<li>Experience with GraphQL</li>\n" +
+            "<li>Experience with VueJs / NuxtJs is a plus</li>\n" +
+            "<li>Good communication & problem-solving skills</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Benefits:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Lunch</li>\n" +
+            "<li>Festival bonus</li>\n" +
+            "<li>Yearly salary review</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Number of vacancy</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>01</li>\n" +
+            "</ul>",
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Expert knowledge of NodeJS - ExpressJS & NestJS</li>\n" +
+            "<li>Highly skilled in NoSQL databases especially in MongoDB</li>\n" +
+            "<li>Use data modeling skills to enhance DB and Cloud Storage layer</li>\n" +
+            "<li>Develop design and testing documentation</li>\n" +
+            "<li>Expert understanding of GIT and Github</li>\n" +
+            "<li>Ability to work autonomously and as part of a team</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "40,000 - 70,000",
+        "location": "Gulshan-2, Dhaka",
+        "mail_to": "career@jatri.co",
+    },
+    {
+        "title": "DevOps Engineer",
+        "slug": "devops-engineer",
+        "iconPath": "job_circular/team_lead.jpg",
+        "deadline": "25<sup>th</sup> June, 2023",
+        "description":
+            "<p class=\"font-thin body_text mt-4\"><u>Job Responsibilities:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Design, implement, and maintain our cloud infrastructure (AWS) and deployment pipelines</li>\n" +
+            "<li>Ensure high availability, scalability, and security of our infrastructure</li>\n" +
+            "<li>Implement and manage monitoring, logging, and alerting systems</li>\n" +
+            "<li>Work with development teams to automate software delivery processes</li>\n" +
+            "<li>Continuously improve the infrastructure and deployment processes to increase efficiency and reduce downtime</li>\n" +
+            "<li>Develop and maintain documentation for the infrastructure and deployment processes</li>\n" +
+            "<li>Participate in on-call rotation to support critical production issues</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Education & Experience:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>4+ years of experience as a DevOps Engineer or a similar role</li>\n" +
+            "<li>Experience with cloud infrastructure and services (AWS)</li>\n" +
+            "<li>Proven experience with automation tools (Ansible, Chef, Puppet, Terraform)</li>\n" +
+            "<li>Experience with continuous integration and deployment (CI/CD) pipelines</li>\n" +
+            "<li>Experience with monitoring and logging tools (Prometheus, Grafana, ELK stack)</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Additional Requirements:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Strong scripting skills (Python, Bash, or similar)</li>\n" +
+            "<li>Strong understanding of containerization and orchestration technologies (Docker, Kubernetes)</li>\n" +
+            "<li>Strong problem-solving skills and ability to troubleshoot complex issues</li>\n" +
+            "<li>Good communication skills and ability to work in a collaborative environment</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Benefits:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Lunch</li>\n" +
+            "<li>Festival bonus</li>\n" +
+            "<li>Yearly salary review</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Number of vacancy</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>01</li>\n" +
+            "</ul>",
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Design, implement, and maintain our cloud infrastructure (AWS) and deployment pipelines</li>\n" +
+            "<li>Ensure high availability, scalability, and security of our infrastructure</li>\n" +
+            "<li>Implement and manage monitoring, logging, and alerting systems</li>\n" +
+            "<li>Work with development teams to automate software delivery processes</li>\n" +
+            "<li>Continuously improve the infrastructure and deployment processes to increase efficiency and reduce downtime</li>\n" +
+            "<li>Develop and maintain documentation for the infrastructure and deployment processes</li>\n" +
+            "<li>Participate in on-call rotation to support critical production issues</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "80,000 - 1,40,000",
+        "location": "Gulshan-2, Dhaka",
+        "mail_to": "career@jatri.co",
+    },{
+        "title": "SQA Engineer",
+        "slug": "sqa-engineer",
+        "iconPath": "job_circular/team_lead.jpg",
+        "deadline": "25<sup>th</sup> June, 2023",
+        "description":
+            "<p class=\"font-thin body_text mt-4\"><u>Job Responsibilities:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Analyze project requirements and product objectives to create, maintain, and execute a suite of test cases</li>\n" +
+            "<li>Estimate test efforts, analyze and communicate test results, track defects and participate in a day-to-day QA activity</li>\n" +
+            "<li>Manage the final quality of production release</li>\n" +
+            "<li>Troubleshoot quality issues and modification of test procedures</li>\n" +
+            "<li>Test planning, prioritizing, and coordination efforts</li>\n" +
+            "<li>Expertise in breaking down feature requirements to design efficient targeted & end-to-end testing scenarios</li>\n" +
+            "<li>Experience with a variety of testing frameworks including Cypress, Jest, Selenium, Pytest, Junit, etc.</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Experience:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>B.Sc. in Computer Science & Engineering / Software Engineering or equivalent degree</li>\n" +
+            "<li>Educational background is flexible for a highly skilled person</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Benefits:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Lunch</li>\n" +
+            "<li>Festival bonus</li>\n" +
+            "<li>Yearly salary review</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Number of vacancy</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>02</li>\n" +
+            "</ul>",
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Analyze project requirements and product objectives to create, maintain, and execute a suite of test cases</li>\n" +
+            "<li>Estimate test efforts, analyze and communicate test results, track defects and participate in a day-to-day QA activity</li>\n" +
+            "<li>Manage the final quality of production release</li>\n" +
+            "<li>Troubleshoot quality issues and modification of test procedures</li>\n" +
+            "<li>Test planning, prioritizing, and coordination efforts</li>\n" +
+            "<li>Expertise in breaking down feature requirements to design efficient targeted & end-to-end testing scenarios</li>\n" +
+            "<li>Experience with a variety of testing frameworks including Cypress, Jest, Selenium, Pytest, Junit, etc.</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "30,000 - 60,000",
+        "location": "Gulshan-2, Dhaka",
+        "mail_to": "career@jatri.co",
+    },
+    {
+        "title": "Software Engineer (Android)",
+        "slug": "software-engineer-android",
+        "iconPath": "job_circular/team_lead.jpg",
+        "deadline": "25<sup>th</sup> June, 2023",
+        "description":
+            "<p class=\"font-thin body_text mt-4\"><u>Job Responsibilities:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Working on our Android applications to implement new features & drive improvements to the existing codebase</li>\n" +
+            "<li>Take ownership and have an impact on the product.</li>\n" +
+            "<li>Engage in knowledge-sharing within your team as well as beyond</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Additional Requirements:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Significant Android engineering experience (2+ years), with experience working on at least two large native Android applications</li>\n" +
+            "<li>Experience programming in Kotlin</li>\n" +
+            "<li>Proven work experience in MVVM structural design patterns</li>\n" +
+            "<li>Robust knowledge of RxJava, Navigation Component, Dependency Injection (Dagger or Hilt), and Modularization.</li>\n" +
+            "<li>Deep understanding of Leakcanary, Retrofit, Room, work manager, View-Binding, Kotlin Coroutines, and Navigation component.</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Education & Experience:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>B.Sc. in Computer Science & Engineering / Software Engineering or Equivalent Degree</li>\n" +
+            "<li>Educational background is flexible for a highly skilled person</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Benefits:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Lunch</li>\n" +
+            "<li>Festival bonus</li>\n" +
+            "<li>Yearly salary review</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Number of vacancy</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>01</li>\n" +
+            "</ul>",
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Working on our Android applications to implement new features & drive improvements to the existing codebase</li>\n" +
+            "<li>Take ownership and have an impact on the product.</li>\n" +
+            "<li>Engage in knowledge-sharing within your team as well as beyond</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "40,000 - 60,000",
+        "location": "Gulshan-2, Dhaka",
+        "mail_to": "career@jatri.co",
+    }
 ]
+
+export default careerList;
